5c27d9015a4a5b3807a2c3164b7143219cfe43e6
Kevin <ndw930719@163.com>
2025-01-06 17:23:44 +0800

    common-pom

---
+++ edu-common/src/main/java/com/atguigu/edu/realtime/common/constant/Constant.java
@@ -0,0 +1,72 @@
+package com.atguigu.edu.realtime.common.constant;
+
+/* *
+ * Package Name: com.atguigu.gmall.realtime.common.constant
+ * Author : Kevin
+ * Create Date ：2024/12/20
+ * Create Time ：11:43
+ * <p>
+ * version: 0.0.1.0
+ */
+
+
+public class Constant {
+
+
+
+    public static final String REDIS_HOST = "hadoop103";
+    public static final int REDIS_PORT = 6379;
+    public static final int REDIS_DIM_EX_ONE_DAY = 24 * 60 * 60 * 1000;
+
+
+    public static final String MAXWELL_TYPE_DELETE = "delete";
+    public static final String MAXWELL_TYPE_INSERT = "insert";
+    public static final String MAXWELL_TYPE_UPDATE = "update";
+    public static final String MAXWELL_TYPE_BOOTSTRAP_INSERT = "bootstrap-insert";
+
+    public static final String HBASE_ZOOKEEPER_QUORUM = "hadoop101,hadoop102,hadoop104";
+
+    public static final String HBASE_NAMESPACE_DIM = "edu_realtime";
+
+
+    public static final String CDC_OP_C = "c";
+    public static final String CDC_OP_D = "d";
+    public static final String CDC_OP_R = "r";
+    public static final String CDC_OP_U = "u";
+    public static final String DB_NAME = "edu";
+    public static final String CONFIG_DB_NAME = "edu_config";
+    public static final String TABLE_PROCESS_DIM = "table_process_dim";
+    public static final String TABLE_PROCESS_DWD = "table_process_dwd";
+
+
+    public static final String CK_PATH_PREFIX = "hdfs://hadoop102:8020/edu/stream/";
+
+    public static final String HDFS_USER_NAME_CONFIG = "HADOOP_USER_NAME";
+    public static final String HDFS_USER_NAME_VALUE = "atguigu";
+
+    public static final String DORIS_FENODES = "hadoop102:7030";
+    public static final String DORIS_DB_NAME = "edu_realtime";
+    public static final String DORIS_USERNAME = "root";
+    public static final String DORIS_PASSWORD = "000000";
+
+    public static final String KAFKA_BROKERS = "hadoop105:9092,hadoop103:9092,hadoop104:9092";
+
+    public static final String TOPIC_DB = "topic_db";
+    public static final String TOPIC_LOG = "topic_log";
+
+    public static final String MYSQL_HOST = "hadoop101";
+    public static final int MYSQL_PORT = 3306;
+    public static final String MYSQL_USER_NAME = "root";
+    public static final String MYSQL_PASSWORD = "000000";
+    public static final String HBASE_NAMESPACE = "edu_realtime";
+
+    public static final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String MYSQL_URL = "jdbc:mysql://hadoop102:3306?useSSL=false";
+    public static final String MYSQL_URL_CONFIG = "jdbc:mysql://hadoop102:3306/edu_config";
+
+    public static final String TOPIC_DWD_TRAFFIC_DIRTY = "dwd_traffic_dirty";
+
+
+    public static final String DWS_TRAFFIC_SOURCE_KEYWORD_PAGE_VIEW_WINDOW = "dws_traffic_source_keyword_page_view_window";
+
+}
--- edu-common/src/main/java/com/atguigu/edu/realtime/common/util/HBaseUtil.java
+++ edu-common/src/main/java/com/atguigu/edu/realtime/common/util/HBaseUtil.java
@@ -5,7 +5,6 @@ package com.atguigu.edu.realtime.common.util;
  * Author : Kevin
  * Create Date ：2025/1/6
  * Create Time ：17:00
- * TODO
  * <p>
  * version: 0.0.1.0
  */
@@ -26,6 +25,30 @@ import java.util.stream.Collectors;
 
 
 public class HBaseUtil {
+
+
+
+    /***
+     * 获取异步连接
+     */
+    public static AsyncConnection getAsyncConnection()  {
+        // 1. 创建配置对象
+        Configuration conf = HBaseConfiguration.create();
+
+        // 2. 添加配置参数
+        conf.set("hbase.zookeeper.quorum",HBASE_ZOOKEEPER_QUORUM );
+
+        // 3. 创建hbase的连接
+        // 默认使用同步连接
+        AsyncConnection asyncConnection;
+        try {
+            asyncConnection = ConnectionFactory.createAsyncConnection(conf).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("获取 HBase AsyncConnection 失败");
+        }
+        return asyncConnection;
+    };
 
     /***
      * 获取连接
@@ -49,4 +72,252 @@ public class HBaseUtil {
         return connection;
     };
 
+    /**
+     * 关闭连接
+     */
+    public static void closeConnection(Connection connection) {
+
+        if( connection != null || !connection.isClosed() ) {
+
+            try {
+                connection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("关闭 HBase Connection 失败");
+            }
+        }
+    };
+    /**
+     * 关闭连接
+     */
+    public static void closeAsyncConnection(AsyncConnection asyncConnection) {
+
+        if( asyncConnection != null || !asyncConnection.isClosed() ) {
+
+            try {
+                asyncConnection.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("关闭 HBase Connection 失败");
+            }
+        }
+    };
+
+    /***
+     *
+     * @param connection    HBase 连接
+     * @param namespaceName 表空间
+     * @param tableName     表名
+     * @param cfs           列族名
+     */
+    public static void createTable( Connection connection, String namespaceName, String tableName, String  cfs ) {
+        Admin admin = null;
+        // 获取 Admin 对象
+        try {
+            admin = connection.getAdmin();
+
+            TableName tn = TableName.valueOf( namespaceName, tableName );
+            // 判断表是否存在
+            if (admin.tableExists(tn)) {
+                System.out.println( " HBase table " + namespaceName + "." + tableName + " already exists!");
+                return;
+            }
+
+            TableDescriptor tableDescriptor = TableDescriptorBuilder.newBuilder(tn)
+                    .setColumnFamily(
+
+                            ColumnFamilyDescriptorBuilder.of(Bytes.toBytes(cfs))
+                    )
+                    .build();
+
+            admin.createTable(tableDescriptor);
+
+            System.out.println( " HBase table " + namespaceName + "." + tableName + " created! " );
+
+            admin.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("HBase建表失败");
+        }finally {
+            if( admin != null ){
+                try {
+                    admin.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException( "关闭 Admin 失败 " );
+
+                }
+            }
+
+        }
+    }
+
+    public static void dropTable(Connection connection, String namespaceName, String tableName){
+        Admin admin = null;
+        // 获取 Admin 对象
+        try {
+            admin = connection.getAdmin();
+
+            TableName tn = TableName.valueOf(namespaceName,tableName);
+            // 判断表是否存在
+            if ( !admin.tableExists( tn ) )  {
+                System.out.println( " HBase table " + namespaceName + "." + tableName + " not exists!");
+                return;
+            }
+
+            // 先 disable
+            admin.disableTable( tn );
+
+            // 再 delete
+            admin.deleteTable( tn );
+
+            System.out.println( " HBase table " + namespaceName + "." + tableName + " deleted! " );
+
+            admin.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("HBase删表失败");
+        }finally {
+            if( admin != null ){
+                try {
+                    admin.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException( "关闭 Admin 失败 " );
+
+                }
+            }
+
+        }
+    }
+
+    /**
+     * 写入数据
+     * shell: put 'namespace:tableName' , 'rk' , 'cf:cl' , 'value'
+     */
+    public static void putRow(Connection connection, String namespaceName, String tableName, String rowKey,String cf, JSONObject dataJson) throws IOException {
+
+        try {
+            TableName tn = TableName.valueOf( namespaceName, tableName );
+            Table table = connection.getTable( tn );
+
+            // 将 dataJson 处理成 put 集合
+            List<Put> putList = dataJson
+                    .entrySet()
+                    .stream()
+                    .filter(entry -> entry.getValue() != null) // 过滤空值
+                    .map(entry -> {
+                        Put put = new Put( Bytes.toBytes(rowKey) );
+                        put.addColumn( Bytes.toBytes(cf), Bytes.toBytes(entry.getKey()), Bytes.toBytes(entry.getValue().toString()));
+                        return put;
+                    })
+                    .collect(Collectors.toList());
+
+            table.put( putList );
+
+            System.out.println( " ➕ HBase 成功写入 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据。" );
+            table.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println( " ⚠️ HBase 写入 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据失败！⚠️" );
+        }
+    }
+
+    /**
+     * 删除数据
+     */
+    public static void delRow(Connection connection, String namespaceName, String tableName, String rowKey){
+
+        try {
+            TableName tn = TableName.valueOf( namespaceName, tableName );
+            Table table = connection.getTable( tn );
+
+            Delete delete = new Delete(Bytes.toBytes(rowKey));
+            table.delete(delete);
+            System.out.println( " ➖ HBase 成功删除 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据。" );
+
+            table.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println( " ⚠️ HBase 删除 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据失败！⚠️" );
+
+        }
+    }
+
+    /**
+     * 查询数据
+     */
+    public static JSONObject getRow(Connection connection, String namespaceName, String tableName, String rowKey ){
+
+        try {
+            TableName tn = TableName.valueOf( namespaceName, tableName );
+            Table table = connection.getTable( tn );
+            Get get = new Get(Bytes.toBytes(rowKey));
+            Result result = table.get(get);
+
+            // 将查询结果封装成 JSONObject 对象
+            JSONObject jsonObject = new JSONObject();
+
+            List<Cell> cells = result.listCells();
+            for (Cell cell : cells) {
+                String k = Bytes.toString(CellUtil.cloneQualifier(cell));
+                String v = Bytes.toString(CellUtil.cloneValue(cell));
+
+                jsonObject.put(k,v);
+            }
+            table.close();
+            System.out.println( "  HBase 查询 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据: " + jsonObject );
+            return jsonObject;
+
+        }catch (Exception e){
+            e.printStackTrace();
+//            System.out.println( " ⚠️ HBase 查询 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据失败！⚠️" );
+//            closeConnection(connection);
+//            return null;// 🐞🐞
+            throw new RuntimeException("  ⚠\uFE0F HBase 查询 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据失败！⚠\uFE0F");
+        }
+    }
+    /**
+     * 查询数据
+     */
+    public static JSONObject getRowAsync(AsyncConnection asyncConnection, String namespaceName, String tableName, String rowKey ){
+
+        try {
+            TableName tn = TableName.valueOf( namespaceName, tableName );
+            AsyncTable<AdvancedScanResultConsumer> asyncTable = asyncConnection.getTable(tn);
+            Get get = new Get(Bytes.toBytes(rowKey));
+            Result result = asyncTable.get(get).get();
+
+            // 将查询结果封装成 JSONObject 对象
+            JSONObject jsonObject = new JSONObject();
+
+            List<Cell> cells = result.listCells();
+
+            for (Cell cell : cells) {
+                String k = Bytes.toString(CellUtil.cloneQualifier(cell));
+                String v = Bytes.toString(CellUtil.cloneValue(cell));
+
+                jsonObject.put(k,v);
+            }
+//            asyncTable.close();
+            System.out.println( "  HBase 查询 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据: " + jsonObject );
+            return jsonObject;
+
+        }catch (Exception e){
+            e.printStackTrace();
+//            System.out.println( " ⚠️ HBase 查询 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据失败！⚠️" );
+//            closeConnection(connection);
+//            return null;// 🐞🐞
+            throw new RuntimeException("  ⚠\uFE0F HBase 查询 " + namespaceName + "." + tableName + " 表，rowkey 为 " + rowKey + " 的数据失败！⚠\uFE0F");
+        }
+    }
+
+    public static void main(String[] args) {
+        AsyncConnection asyncConnection = getAsyncConnection();
+        JSONObject gmallRealtime = getRowAsync(asyncConnection, "gmall_realtime", "", "1");
+        System.out.println(gmallRealtime);
+        closeAsyncConnection(asyncConnection);
+    }
 }
