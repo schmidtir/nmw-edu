fccf69fc4e31a7fa022e76ee2c0e581a76666bd4
南风过境 <wzy13363448722@163.com>
2025-01-06 16:57:46 +0800

    update

---
+++ edu-common/src/main/java/com/atguigu/edu/realtime/common/util/FlinkSqlUtil.java
@@ -0,0 +1,12 @@
+package com.atguigu.edu.realtime.common.util;
+
+/**
+ * Package Name: com.atguigu.edu.realtime.common.util
+ * Author: WZY
+ * Create Date: 2025/1/6
+ * Create Time: 下午4:57
+ * Vserion : 1.0
+ * TODO
+ */
+public class FlinkSqlUtil {
+}
