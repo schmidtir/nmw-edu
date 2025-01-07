136a440b7aa495808442988cd00f7c540e8fd37d
南风过境 <wzy13363448722@163.com>
2025-01-07 14:25:20 +0800

    update

--- edu-dwd/edu-dwd-base-log/src/main/java/com/atguigu/edu/realtime/dwd/app/DwdBaseLogApp.java
+++ edu-dwd/edu-dwd-base-log/src/main/java/com/atguigu/edu/realtime/dwd/app/DwdBaseLogApp.java
@@ -1,8 +1,18 @@
 package com.atguigu.edu.realtime.dwd.app;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.atguigu.edu.realtime.common.base.BaseApp;
 import com.atguigu.edu.realtime.common.constant.Constant;
+import com.atguigu.edu.realtime.common.util.FlinkSinkUtil;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SideOutputDataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
 
 /**
  * Package Name: com.atguigu.edu.realtime.dwd.app
@@ -18,6 +28,30 @@ public class DwdBaseLogApp extends BaseApp{
     }
     @Override
     public void handle(StreamExecutionEnvironment env, DataStreamSource<String> ds) {
+        // 脏数据 Tag
+        OutputTag<String> dirtyTag = new OutputTag<>("dirtyTag", Types.STRING);
+        SingleOutputStreamOperator<JSONObject> etlDs = ds.process(
+                new ProcessFunction<String, JSONObject>() {
+                    @Override
+                    public void processElement(String value, ProcessFunction<String, JSONObject>.Context ctx, Collector<JSONObject> out) throws Exception {
+                        try {
+                            // 转换成 JSONOBJ
+                            JSONObject jsonObj = JSON.parseObject(value);
+                            // 写出
+                            out.collect(jsonObj);
+                        } catch (Exception e) {
+                            System.out.println("Log分流过滤脏数据：" + value);
+                            // 将脏数据写到侧输出流
+                            ctx.output(dirtyTag, value);
+                        }
+                    }
+                }
+        );
 
+        // 捕获脏数据的侧输出流
+        SideOutputDataStream<String> dirtyDs = etlDs.getSideOutput(dirtyTag);
+        // 将脏数据写入 Kafka 对应的主题中
+        KafkaSink<String> kafkaSink = FlinkSinkUtil.getKafkaSink(Constant.TOPIC_DWD_TRAFFIC_DIRTY);
+        dirtyDs.sinkTo( kafkaSink );
     }
 }
