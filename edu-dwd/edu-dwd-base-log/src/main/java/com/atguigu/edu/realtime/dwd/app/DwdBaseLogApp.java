b2683603fa7634d5f2f480791448bf3247e98dc2
南风过境 <wzy13363448722@163.com>
2025-01-07 14:04:41 +0800

    update

---
+++ edu-dwd/edu-dwd-base-log/src/main/java/com/atguigu/edu/realtime/dwd/app/DwdBaseLogApp.java
@@ -0,0 +1,12 @@
+package com.atguigu.edu.realtime.dwd.app;
+import com.atguigu.edu.realtime.common.base.BaseApp;
+/**
+ * Package Name: com.atguigu.edu.realtime.dwd.app
+ * Author: WZY
+ * Create Date: 2025/1/7
+ * Create Time: 下午2:00
+ * Vserion : 1.0
+ * TODO
+ */
+public class DwdBaseLogApp extends BaseApp{
+}
