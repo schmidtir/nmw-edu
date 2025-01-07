fa0d50a0ab4950406a7471df61d064a982b8d8de
南风过境 <wzy13363448722@163.com>
2025-01-07 14:06:25 +0800

    update

--- edu-dwd/edu-dwd-base-log/src/main/java/com/atguigu/edu/realtime/dwd/app/DwdBaseLogApp.java
+++ edu-dwd/edu-dwd-base-log/src/main/java/com/atguigu/edu/realtime/dwd/app/DwdBaseLogApp.java
@@ -1,5 +1,9 @@
 package com.atguigu.edu.realtime.dwd.app;
 import com.atguigu.edu.realtime.common.base.BaseApp;
+import com.atguigu.edu.realtime.common.constant.Constant;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 /**
  * Package Name: com.atguigu.edu.realtime.dwd.app
  * Author: WZY
@@ -9,4 +13,11 @@ import com.atguigu.edu.realtime.common.base.BaseApp;
  * TODO
  */
 public class DwdBaseLogApp extends BaseApp{
+    public static void main(String[] args) {
+        new DwdBaseLogApp().start(10011 , 3 , "dwd_base_log_app" , Constant.TOPIC_LOG);
+    }
+    @Override
+    public void handle(StreamExecutionEnvironment env, DataStreamSource<String> ds) {
+
+    }
 }
